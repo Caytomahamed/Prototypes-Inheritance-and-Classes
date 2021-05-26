6148ba2e0e380be4166cfc2a2ff20e2dcbe03b10
Aitoodeveloper <aidarous1143@gmail.com>
2021-05-26 23:30:14 +0300

    added files

--- classes.js
+++ classes.js
@@ -1,10 +1,81 @@
 // In this assignment, you will be creating a class named "Person" and then create your whole family from that class.
 
-
-
 // Create "Person" class
 // Give it "Name", "Age", "Hobby" properties
 // Also give it a method "Speak", which says "Hello, My name is + name + and I'm + age + years old. I love + Hobby"
 
+// class Animal {
+//   constructor(attr) {
+//     this.name = attr.name;
+//     this.location = attr.location;
+//     this.work = attr.work
+//   }
+  
+//   job(){
+//     return `${this.name} is a ${this.work}`;
+//   }
+// }
 
+// const fartun = new Animal ({
+//   name: "Fartun",
+//   location: "Minnesota",
+//   work: "Nurse"
+// })
+
+// console.log(fartun.job())
+class Person{
+
+    constructor(attr){
+        this.Name = attr.Name;
+        this.Age =attr.Age;
+        this.Hobby =attr.Hobby;
+
+    }
+    Speak(){
+        return `Hello, My name is ${this.Name}  and I'm ${this.Age} age + years old. I love ${this.Hobby}`
+    }
+}
+
+const father = new Person({
+    Name :'Mohamed',
+    Age :60 ,
+    Hobby: 'Politics'
+})
+console.log(father.Speak());
+
+
+const mother = new Person({
+    Name :'Hibo',
+    Age :50,
+    Hobby: 'Cooking'
+})
+console.log(mother.Speak());
+
+const brotherone = new Person({
+    Name :'Mouse',
+    Age :30 ,
+    Hobby: 'Arts'
+})
+console.log(brotherone.Speak());
+
+const brTwo = new Person({
+    Name :'Sharmarke',
+    Age :20 ,
+    Hobby: 'Football'
+})
+console.log(brTwo.Speak());
+
+const sisterOne = new Person({
+    Name :'Sucaad ',
+    Age :15,
+    Hobby: 'Computer science'
+})
+console.log(sisterOne.Speak());
+
+const sisterTwo= new Person({
+    Name :'Maryan',
+    Age :25 ,
+    Hobby: 'writing '
+})
+console.log(sisterTwo.Speak());
 // Extend Person class and create one class for each member of your family.
--- this.js
+++ this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. waxa weeye ka ugu fudud uguna isticmaal kayare leh shuruucda this 
+* 2. waxa ay utaa ganta sharcigan this ee labaad parents ka kore objects js ama hal ka ay kujirto hal parent u ah ayay  utaagantany 
+* 3. waxay isku xidhaa fuction IYO  objects cusub si blueprint ahaan loo isticmaalaayo fuction 
+* 4. waxay ay isku xidhaa fucntion iyo object 
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +12,43 @@
 // Principle 1
 
 // code example for Window Binding
-
+function name(magav){
+    console.log(this.magac);
+}
+console.log(name());
 // Principle 2
 
 // code example for Implicit Binding
+const car ={
+   name : 'BMW',
+   version: 200-12-1;
+   prodectBy :'tasla',
+   customer:function(){
+       return `${this.name} its price is 2 million `
+   }
+}
+
+console.log(car.console());
 
 // Principle 3
 
 // code example for New Binding
+function car (carName, varsion){
+    this.carName = carName;
+    this.version =version;
+}
+const saleCar = new car ('BMW', '2021-0-1')
+console.log(saleCar);
 
 // Principle 4
 
 // code example for Explicit Binding
+function country() {
+    console.log(this.nation);
+}
+const state ={
+    nation:'canada'
+}
+
+country.call(state)
+console.log(country());
